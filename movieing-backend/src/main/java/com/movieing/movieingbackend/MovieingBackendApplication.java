f3f71cdd3fa1eecc593c8096fcaeec49a46daef4
huch99 <huichan58@naver.com>
2026-01-08 17:30:53 +0900

    프로젝트 생성

---
+++ movieing-backend/src/main/java/com/movieing/movieingbackend/MovieingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.movieing.movieingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieingBackendApplication.class, args);
+    }
+
+}
---
+++ movieing-backend/src/test/java/com/movieing/movieingbackend/MovieingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieing.movieingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
